7aa779e48aad7b1338b780eccf22522291a9abed
Zamzamke <zamzamchelimo@gmail.com>
2023-06-15 13:06:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program shows the use of puts function to print out a message
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
