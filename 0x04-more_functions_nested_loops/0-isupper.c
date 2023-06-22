3f699a076d57d39c98eadebebe51f3c78ebdca69
Zamzamke <zamzamchelimo@gmail.com>
2023-06-22 15:48:26 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the int to be checked
+ * @c: the char to check
  * Return:(1) if char is uppercase,0 otherwise
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
